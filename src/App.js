5596e8f6fcffc80324800511aafa54d8ece9bbac
Eric Vonk <mcvenco@gmail.com>
2022-11-30 20:14:10 +0100

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './css/App.css';
+import React from 'react';
 import Map from './components/Map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Fungus Friends! <img src="/images/logo_outline_64.png" alt="Fungus Friends logo" />
-        </h1>
-      </header>
-      <Map />
-    </div>
-  );
+class App extends React.Component {
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Fungus Friends! <img src="/images/logo_outline_64.png" alt="Fungus Friends logo" />
+          </h1>
+        </header>
+        <Map />
+      </div>
+    );
+  }
 }
 
 export default App;

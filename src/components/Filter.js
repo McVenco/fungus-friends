4066d9a8021f569f530b9ea17223474ccab22b2b
Eric Vonk <mcvenco@gmail.com>
2022-12-06 13:52:17 +0100

    Put colors and spots as select options

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,22 +4,34 @@ import { Color, Spots } from '../front-end-api.ts';
 class Filter extends React.Component {
   
   render () {
+    // filter Color/Spots enum from ts file to display only string values:
+    // create helper to remove numbers
+    const filterNumber = value => isNaN(Number(value)) === false;
+    // turn enums into arrays
+    function colorToArray(Color) {
+      return Object.values(Color)
+        .filter(filterNumber)
+        .map(key => Color[key])
+    }
+    function spotsToArray(Spots) {
+      return Object.values(Spots)
+        .filter(filterNumber)
+        .map(key => Spots[key])
+    }
+    // return string values to select options
+    const colorValues = Object.values(colorToArray(Color)).map(col => <option key={col}>{col}</option>)
+    const spotsValues = Object.values(spotsToArray(Spots)).map(spot => <option key={spot}>{spot}</option>)
 
     return (
       <>
         <h3>Filter fungi</h3>
         <select name="colorfilter" id="">
           <option default>- filter color -</option>
-          {/* loop hier over de kleuren in Color */}
-          {Object.keys(Color).map(key => (
-            <option value={Color[key]}>{Color[key]}</option>
-          ))}
+          {colorValues}
         </select>
         <select name="spotsfilter" id="">
           <option default>- filter spots -</option>
-          {Object.keys(Spots).map(key => (
-            <option value={Spots[key]}>{Spots[key]}</option>
-          ))}
+          {spotsValues}
         </select>
       </>
     )

d352b477bfa016849f15e56483d13834c0362778
Eric Vonk <mcvenco@gmail.com>
2022-12-07 20:15:02 +0100

    Throw in some styling

--- src/components/App.js
+++ src/components/App.js
@@ -29,20 +29,26 @@ class App extends React.Component {
 
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Fungus Friends! <img src="/images/logo_outline_64.png" alt="Fungus Friends logo" />
-          </h1>
-        </header>
-        <div className="filter">
-          <Filter
-            fungi={this.state.fungi}
-            updateFilter={this.updateFilter}
-          />
-        </div>
-        <div className="map">
-         <Map fungi={this.state.fungi} />
+      <div className="App" style={{
+          backgroundImage: "url(/images/fungi-background.jpeg)",
+          height: '100vh',
+          width: '100vw',
+        }}>
+        <div className="App-container">
+          <header className="App-header">
+            <h1>
+              Fungus Friends! <img src="/images/logo_outline_64.png" alt="Fungus Friends logo" />
+            </h1>
+          </header>
+          <div className="App-left">
+            <Filter
+              fungi={this.state.fungi}
+              updateFilter={this.updateFilter}
+              />
+          </div>
+          <div className="App-right">
+          <Map fungi={this.state.fungi} />
+          </div>
         </div>
       </div>
     );
